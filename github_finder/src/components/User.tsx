e00784ec6e3558b3d6175f9972bcbc0085702450
Cristian Melo <cristianmeelo@gmail.com>
2022-12-31 14:13:43 -0300

    🚧 implemented User styles

--- github_finder/src/components/User.tsx
+++ github_finder/src/components/User.tsx
@@ -3,31 +3,42 @@ import { UserProps } from "../types/user";
 
 import { MdLocationPin } from "react-icons/md";
 
+import classes from "./User.module.css";
+
+import React from "react";
+
 export const User = ({
   login,
   avatar_url,
   followers,
   following,
   location,
-}: UserProps) => (
-  <div>
-    <img src={avatar_url} alt={login} />
-    <h2>{login}</h2>
-    <p>
-      <MdLocationPin />
-    </p>
-    <div>
+}: UserProps) => {
+  return (
+    <div className={classes.user}>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && (
+        <p className={classes.location}>
+          <MdLocationPin />
+          <span>{location}</span>
+        </p>
+      )}
+
+      <div className={classes.stats}>
         <div>
-            <p>Seguidores:</p>
-            <p>{followers}</p>
+          <p>Seguidores:</p>
+          <p className={classes.number}>{followers}</p>
         </div>
-        <div> 
-            <p>Seguindo:</p>
-            <p>{following}</p>
+        <div>
+          <p>Seguindo:</p>
+          <p className={classes.number}>{following}</p>
         </div>
-    </div>
-    <Link to={`/repos/${login}`}>
+      </div>
+
+      <a href={`https://github.com/${login}?tab=repositories`}>
         Ver melhores projetos
-    </Link>
-  </div>
-);
+      </a>
+    </div>
+  );
+};
