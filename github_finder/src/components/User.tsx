01acafa743966ebd92541e2bc08ea313e8d3d214
cristianmeelo <cristianmeelo@gmail.com>
2023-05-11 13:22:49 -0300

    Update User.tsx

--- github_finder/src/components/User.tsx
+++ github_finder/src/components/User.tsx
@@ -1,44 +1,32 @@
-import { Link } from "react-router-dom";
-import { UserProps } from "../types/user";
+import React from "react"
+import { Link } from "react-router-dom"
+import { UserProps } from "../types/user"
+import { MdLocationPin } from "react-icons/md"
+import classes from "./User.module.css"
 
-import { MdLocationPin } from "react-icons/md";
+export const User = ({ login, avatar_url, followers, following, location }: UserProps) => {
+	return (
+		<div className={classes.user}>
+			<img src={avatar_url} alt={login} />
+			<h2>{login}</h2>
+			{location && (
+				<p className={classes.location}>
+					<MdLocationPin />
+					<span>{location}</span>
+				</p>
+			)}
+			<div className={classes.stats}>
+				<div>
+					<p>Seguidores:</p>
+					<p className={classes.number}>{followers}</p>
+				</div>
+				<div>
+					<p>Seguindo:</p>
+					<p className={classes.number}>{following}</p>
+				</div>
+			</div>
 
-import classes from "./User.module.css";
-
-import React from "react";
-
-export const User = ({
-  login,
-  avatar_url,
-  followers,
-  following,
-  location,
-}: UserProps) => {
-  return (
-    <div className={classes.user}>
-      <img src={avatar_url} alt={login} />
-      <h2>{login}</h2>
-      {location && (
-        <p className={classes.location}>
-          <MdLocationPin />
-          <span>{location}</span>
-        </p>
-      )}
-
-      <div className={classes.stats}>
-        <div>
-          <p>Seguidores:</p>
-          <p className={classes.number}>{followers}</p>
-        </div>
-        <div>
-          <p>Seguindo:</p>
-          <p className={classes.number}>{following}</p>
-        </div>
-      </div>
-
-      <a href={`https://github.com/${login}?tab=repositories`}>
-        Ver melhores projetos
-      </a>
-    </div>
-  );
-};
+			<a href={`https://github.com/${login}?tab=repositories`}>Ver melhores projetos</a>
+		</div>
+	)
+}
